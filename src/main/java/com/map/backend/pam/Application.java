51baae28d01aa4cfa0eb49f25dd8d196707c1f6b
NorangelInprende <norangel@inprende.com>
2023-10-21 09:23:07 -0400

    Inicio

---
+++ src/main/java/com/map/backend/pam/Application.java
@@ -0,0 +1,13 @@
+package com.map.backend.pam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/map/backend/pam/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.map.backend.pam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
